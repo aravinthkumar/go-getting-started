6393e6757ebcbd2412447a688eec3248564f5c3a
Aravinth <aravinthkumar.panchatcharam@gmail.com>
2021-12-25 16:54:03 +0530

    Working with Structs - Sample Struct Type - Initialize a variable of type struct - Different ways to initialize with values

--- main.go
+++ main.go
@@ -3,16 +3,31 @@ package main
 import "fmt"
 
 func main() {
+	// Definition of a struct
+	type user struct {
+		ID        int
+		FirstName string
+		LastName  string
+	}
 
-	// Declare and initialize a map
-	m := map[string]int{"foo": 42, "boo": 45, "coo": 48}
-	fmt.Println(m)
-	fmt.Println(m["foo"])
-	// Change a element in map
-	m["coo"] = 50
-	fmt.Println(m)
-	// Delete a element in map
-	delete(m, "boo")
-	fmt.Println(m)
+	// Initialize a variable with the struct type, following would have initial values
+	var user1 user
+	fmt.Println(user1)
+
+	// Initialize with values
+	user1.ID = 1
+	user1.FirstName = "Aravinth"
+	user1.LastName = "Kumar"
+
+	fmt.Println(user1)
+
+	fmt.Println(user1.FirstName, user1.LastName)
+	// Another way to Declare and initialize a variable with values
+	user2 := user{
+		ID:        2,
+		FirstName: "Rahul",
+		LastName:  "Jain",
+	}
+	fmt.Println(user2)
 
 }
