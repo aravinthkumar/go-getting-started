925166ca7a1a3c8f9db9f7afa2a6d4d87f0128fe
Aravinth <aravinthkumar.panchatcharam@gmail.com>
2021-12-25 12:23:01 +0530

    multiple variable in same statement

--- main.go
+++ main.go
@@ -25,4 +25,12 @@ func main() {
 	c := complex(3, 4)
 	fmt.Println(c)
 
+	// to print the real and imaginary and also decalare two variable in single statement
+	r, im := real(c), imag(c)
+	fmt.Println(r, im)
+
+	//Another example for declaring two variable in same statement.
+	middleName, lastName := "Malik", "Khan"
+	fmt.Println(middleName, lastName)
+
 }
