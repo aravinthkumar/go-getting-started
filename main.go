055374042e9b4211270be77c82778887d4749b67
Aravinth <aravinthkumar.panchatcharam@gmail.com>
2021-12-25 16:25:53 +0530

    Extensive Slice behavior - Array to Slice - Slice independent - Append on slice - Slice option on collections

--- main.go
+++ main.go
@@ -3,15 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	// Array declaration of type int
-	var arr [3]int
-	arr[0] = 1
-	arr[1] = 2
-	arr[2] = 3
-	fmt.Println(arr[2], arr)
 
-	// Another way to declare and initialize
-	arr2 := [3]int{4, 5, 5}
-	fmt.Println(arr2, arr2[1])
+	arr := [3]int{4, 5, 5}
+	fmt.Println(arr, arr[1])
 
+	// Slice is initialized and declared from a array
+	slice := arr[:]
+	fmt.Println(slice)
+
+	// Array and Slice is changed the data is same since they point to the same value from the memory
+	arr[1] = 9
+	slice[2] = 10
+
+	fmt.Println(arr, slice)
+
+	// Slice declared and initialzed without an array
+	slice1 := []int{8, 9, 0}
+	fmt.Println(slice1)
+	// Slice is appended with more element
+	slice1 = append(slice1, 4, 3)
+	fmt.Println(slice1)
+
+	// Slice options on a collections
+	slice2 := slice1[:1]
+	slice3 := slice1[1:2]
+	slice4 := slice1[1:]
+	fmt.Println(slice1, slice2, slice3, slice4)
 }
