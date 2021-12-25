2ae5bbce0c1ad8ab0790a5ce22495d49f9294790
Aravinth <aravinthkumar.panchatcharam@gmail.com>
2021-12-25 17:33:30 +0530

    created a model package for struct types

--- main.go
+++ main.go
@@ -1,33 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github/aravinthkumar/go-getting-started/models"
+)
 
 func main() {
-	// Definition of a struct
-	type user struct {
-		ID        int
-		FirstName string
-		LastName  string
+	u := models.User{
+		ID:        1,
+		FirstName: "Aravinth",
+		LastName:  "Kumar",
 	}
-
-	// Initialize a variable with the struct type, following would have initial values
-	var user1 user
-	fmt.Println(user1)
-
-	// Initialize with values
-	user1.ID = 1
-	user1.FirstName = "Aravinth"
-	user1.LastName = "Kumar"
-
-	fmt.Println(user1)
-
-	fmt.Println(user1.FirstName, user1.LastName)
-	// Another way to Declare and initialize a variable with values
-	user2 := user{
-		ID:        2,
-		FirstName: "Rahul",
-		LastName:  "Jain",
-	}
-	fmt.Println(user2)
-
+	fmt.Println(u)
 }
