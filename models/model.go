8975712567bd8996e6c9785dd4cb259a04df9b89
Aravinth <aravinthkumar.panchatcharam@gmail.com>
2021-12-26 15:33:07 +0530

    Enhanced the model module with the following - AddByUser, - GetByID, - UpdateUser and - DeleteUser

--- models/model.go
+++ models/model.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 type User struct {
 	ID        int
 	FirstName string
@@ -16,8 +21,41 @@ func GetUsers() []*User {
 }
 
 func AddUser(u User) (User, error) {
+	if u.ID != 0 {
+		return User{}, errors.New("new creation should not include id")
+	}
 	u.ID = nextID
 	nextID++
 	users = append(users, &u)
 	return u, nil
+}
+
+func GetUserById(id int) (User, error) {
+
+	for _, v := range users {
+		if id == v.ID {
+			return *v, nil
+		}
+	}
+	return User{}, errors.New("User doesn't exist")
+}
+
+func UpdateUser(u User) (User, error) {
+	for i, v := range users {
+		if u.ID == v.ID {
+			users[i] = &u
+		}
+	}
+
+	return User{}, errors.New("User not found to update")
+
+}
+
+func DeleteUser(id int) error {
+	for i, v := range users {
+		if id == v.ID {
+			users = append(users[:i], users[i+1:]...)
+		}
+	}
+	return fmt.Errorf("User '%v'  not found to delete", id)
 }
